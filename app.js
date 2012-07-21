592e88f4adbb316669149d67f84b7bfdaae28a8f
Gabriel Adomnicai <gabesoft@gmail.com>
2012-07-21 10:51:11 -0700

    index page

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-var flatiron = require('flatiron'),
-    path = require('path'),
-    app = flatiron.app;
+var flatiron = require('flatiron')
+  , fs       = require('fs')
+  , path     = require('path')
+  , jade     = require('jade')
+  , app      = flatiron.app;
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
 
 app.router.get('/', function () {
-  this.res.json({ 'hello': 'world' });
+    var indexFile    = path.join(__dirname, 'pub/views/index.jade')
+      , layoutFile   = path.join(__dirname, 'pub/views/layouts/main.jade')
+      , index        = fs.readFileSync(indexFile, 'utf8')
+      , layout       = fs.readFileSync(layoutFile, 'utf8')
+      , indexLocals  = {}
+      , indexOpts    = {}
+      , layoutLocals = {
+            title: 'Web Chat'
+          , javascript: ''
+          , body: jade.compile(index, indexOpts)(indexLocals)
+        }
+      , layoutOpts = {}
+      , html       = jade.compile(layout, layoutOpts)(layoutLocals);
+
+    this.res.writeHead(200, { 'Content-Type': 'text/html' });
+    this.res.end(html);
 });
 
 app.start(3000);
